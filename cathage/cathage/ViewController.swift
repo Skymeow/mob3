5a2940daacbc65ed7571f68aa3a96126be766160
Sky Xu <skyrocks88xmz@gmail.com>
2017-10-23 18:16:48 -0700

    add apps

---
+++ cathage/cathage/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  cathage
+//
+//  Created by Sky Xu on 10/23/17.
+//  Copyright © 2017 Sky Xu. All rights reserved.
+//
+
+import UIKit
+import ARKit
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+}
+
