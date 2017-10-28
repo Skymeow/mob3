6865b50acf85acd65895da8bb285ec2b61f0e538
Sky Xu <skyrocks88xmz@gmail.com>
2017-10-28 16:56:36 -0700

    finish displaying img

--- cathage/cathage/ViewController.swift
+++ cathage/cathage/ViewController.swift
@@ -26,9 +26,27 @@ class ViewController: UIViewController {
 
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+//        make a sphere by set chamferRadius to be 0.5*length 
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.1/2)
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+//        create a 3 dimensional vector, the 3rd element is how far the object is away from the origin
+        node.position = SCNVector3(0,0,-0.3)
         self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    
+    @IBAction func reset(_ sender: Any) {
+        self.restartSession()
+    }
+//    reset origin point
+    func restartSession(){
+        self.sceneView.session.pause()
+//        remove box node from parent node
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+//        forget about the old origin and make a new one  based on where you are at the moment
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
 }
